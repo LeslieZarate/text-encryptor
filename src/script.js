c7e254ee47b18e610078130d3e16928ecb228ac9
fiorelly <fiorelly30zarate@gmail.com>
2023-05-28 22:27:46 -0500

    add script js

--- src/script.js
+++ src/script.js
@@ -1,12 +1,19 @@
 const textArea = document.querySelector('.text-area');
 const message = document.querySelector('.message');
 const overlay = document.querySelector('.overlay');
+const information = document.getElementById('information')
+const paragraphElement = information.querySelector("p");
+
+const btnEcrypt = document.getElementById('btn-encrypt')
+const btnDecrypt = document.getElementById('btn-decrypt')
+const btnCopy = document.getElementById('btn-copy')
 
 // La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
 // La letra "a" es convertida para "ai"
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
+
 const encryptionCodes = [
     ["e", "enter"],
     ["i", "imes"],
@@ -14,6 +21,18 @@ const encryptionCodes = [
     ["o", "ober"],
     ["u", "ufat"]
 ];
+
+function verifyText(value) {
+    let error = false;
+    for (let i = 0; i < value.length; i++) {
+        code = value.charCodeAt(i);
+        if (!(code >= 97 && code <= 122) || !code == 32 || !code == 241) {
+            error = true
+            break;
+        }
+    }
+    return error;
+}
 
 function encrypt(word) {
     word = word.toLowerCase()
@@ -25,15 +44,6 @@ function encrypt(word) {
     return word
 }
 
-function btnEcrypt() {
-    const encryptedText = encrypt(textArea.value);
-    message.value = encryptedText;
-    textArea.value = "";
-    overlay.style.display = "none";
-    
-}
-
-
 function decrypt(word) {
     word = word.toLowerCase()
     for (let i = 0; i < encryptionCodes.length; i++) {
@@ -44,9 +54,50 @@ function decrypt(word) {
     return word
 }
 
-function btnDecrypt () {
+
+function copy(text) {
+    if (!text == "") {
+        navigator.clipboard.writeText(text)
+            .then(() => {    
+                alert("Texto copiado con exito.");
+            })
+            .catch(() => {
+                alert("Error, el texto no pudo ser copiado.");
+            });
+    }
+}
+
+function copyToClipboard() {
+    const textarea = document.getElementById("myTextarea");
+    textarea.select();
+    document.execCommand("copy");
+  }
+
+
+btnEcrypt.addEventListener("click", () => {
+    const error = verifyText(textArea.value)
+    if (error) {
+        paragraphElement.style.color = "red";
+        return;
+    }
+    const encryptedText = encrypt(textArea.value);
+    message.value = encryptedText;
+    textArea.value = "";
+    paragraphElement.style.color = "#495057";
+    overlay.style.display = "none";
+    btnCopy.style.display = "block";
+
+});
+
+btnDecrypt.addEventListener("click", () => {
+    verifyText(textArea.value)
     const decryptedText = decrypt(textArea.value);
     message.value = decryptedText;
     textArea.value = "";
-    // message.style.backgroundImage = "none";
-}
+});
+
+btnCopy.addEventListener("click", () => {
+    const text = message.value
+    console.log(text);
+    copy(text);
+});
